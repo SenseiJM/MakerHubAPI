5a036f46f520fe328b3649957f403605cce51450
SenseiJM <loudeche.jean-michel@hotmail.fr>
2021-09-21 16:39:49 +0200

    Added Service, Controller and DTO Files for CategorieInterclubs

---
+++ MakerHubAPI/Controllers/CategorieInterclubsController.cs
@@ -0,0 +1,49 @@
+﻿using MakerHubAPI.DTO.CategorieInterclubs;
+using MakerHubAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakerHubAPI.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategorieInterclubsController : ControllerBase {
+
+        private CategorieInterclubsService cService;
+
+        public CategorieInterclubsController(CategorieInterclubsService cService) {
+            this.cService = cService;
+        }
+
+        [HttpPost]
+        public IActionResult AddCategorieInterclubs(CategorieInterclubsAddDTO dto) {
+            cService.Create(dto);
+            return NoContent();
+        }
+
+        [HttpGet("ByID/{id}")]
+        public IActionResult GetOneByID(int id) {
+            return Ok(cService.GetByID(id));
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() {
+            return Ok(cService.GetAll());
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(CategorieInterclubsDetailsDTO dto, int id) {
+            cService.Update(dto, id);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) {
+            cService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ MakerHubAPI/DTO/CategorieInterclubs/CategorieInterclubsDetailsDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakerHubAPI.DTO.CategorieInterclubs {
+    public class CategorieInterclubsDetailsDTO {
+
+        public string Genre { get; set; }
+
+    }
+}
